5626c292de5dcf751ab4ccb4fea61e87490528e1
vasilii <porokh.vs@gmail.com>
2021-09-11 21:34:46 +0300

    add base

--- app_back.js
+++ app_back.js
@@ -1,5 +1,6 @@
 var express = require("express");
-var baseApi = require("./base/baseApi")
+//var baseApi = require("./base/baseApi")
+var dragonBonesBaseApi = require("./base/dragonBonesSlots/dragonBonesBaseApi")
 var bodyParser = require('body-parser')
 var cors = require('cors')
 
@@ -11,33 +12,36 @@ var PORT = 3010
 var app = express();
 var http = require('http').Server(app);
 app.use(express.static('www'))
-app.use(cors({
-    origin: ['http://192.168.0.101:9000']
-}));
+app.use(cors(/*{ origin: ['http://192.168.0.101:9000'] }*/));
 app.use(bodyParser.json())
 
 
 app.post('/api/add-item', (req, res) => {
-    console.log(req.body)
-    baseApi.addStroke(req.body)
-    res.json({msg: '!!!'})
+    dragonBonesBaseApi.saveToBase(req.body, mess => {
+        if (mess[0] !== 'success') {
+            res.json({ mess })
+        } else {
+            res.json({ mess })
+        }
+    })
 })
 
 
+app.post('/api/remove-item', (req, res) => {
+    dragonBonesBaseApi.removeFromBase(req.body, list => {
+        res.json({ list })
+    })
+})
+
+
+app.post('/api/get-list', (req, res) => {
+    dragonBonesBaseApi.getList(req.body, list => {
+        res.json({ list })
+    })
+})
+
 
 app.listen(PORT, IP)
-
-
-
-
-baseApi.readBase()
-setTimeout(() => {
-    baseApi.addStroke('aaaaaa')
-}, 2000)
-setTimeout(() => {
-    baseApi.removeStroke('aaaaaa')
-}, 2000)
-
 
 
 console.log("listen: " + IP + ":" + PORT)
--- base/dragonBonesSlots/dragonBonesBaseApi.js
+++ base/dragonBonesSlots/dragonBonesBaseApi.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var uniqid = require('uniqid')
-console.log(uniqid())
 
 var baseFileName = './base/dragonBonesSlots/base.json'
 var baseScheme = './base/dragonBonesSlots/scheme.json'
@@ -8,7 +7,7 @@ var baseScheme = './base/dragonBonesSlots/scheme.json'
 
 
 
-exports.saveToBase = function (data) {
+exports.saveToBase = function (data, callback) {
     fs.readFile(baseScheme, 'utf8', function (err, fileScheme) {
         if (err) {
             return console.log(err);
@@ -17,35 +16,109 @@ exports.saveToBase = function (data) {
             if (err) {
                 return console.log(err);
             }
-            const newData = prepareNewData(fileScheme, data)
+            const currentContentBase = JSON.parse(fileBase)
+            const currentScheme = JSON.parse(fileScheme)
+            const { mess, newData } = prepareNewData(currentScheme, data)
+
+            if (!mess.length) {
+                currentContentBase['items'].push(newData)
+                fs.writeFileSync(baseFileName, JSON.stringify(currentContentBase, null, 4));
+                callback(['success'])
+            } else {
+                callback(mess)
+            }
         })
-
-
-
-        currentContentBase = JSON.parse(file)
-        console.log(currentContentBase);
     })
 }
 
 
-const prepareNewData = (scheme, data) => {
-    const newData = {}
-    for (let key in scheme) {
-        if (data[key] === "id") {
-
+exports.removeFromBase = function (data, callback) {
+    fs.readFile(baseFileName, 'utf8', function (err, fileBase) {
+        if (err) {
+            return console.log(err);
         }
-    }
+        let currentContentBase = JSON.parse(fileBase)
+
+        currentContentBase['items'] = currentContentBase['items'].filter(item => item.id !== data.id)
+        fs.writeFileSync(baseFileName, JSON.stringify(currentContentBase, null, 4));
+        callback(currentContentBase['items'])
+    })
 }
 
-// exports.addStroke = function (stroke) {
-//     currentContentBase.items.push(stroke)
-//     console.log(currentContentBase)
-//     fs.writeFileSync(baseFileName, JSON.stringify(currentContentBase));
-// }
-//
-//
-// exports.removeStroke = function () {
-//     currentContentBase.items.splice(-1, 1)
-//     console.log(currentContentBase)
-//     fs.writeFileSync(baseFileName, JSON.stringify(currentContentBase));
-// }
+
+exports.getList = function (data, callback) {
+    fs.readFile(baseFileName, 'utf8', function (err, fileBase) {
+        if (err) {
+            return console.log(err);
+        }
+        const currentContentBase = JSON.parse(fileBase)
+        callback(currentContentBase.items)
+    })
+}
+
+
+
+const prepareNewData = (scheme, data) => {
+    const newData = {}
+    const mess = []
+
+    for (let key in scheme) {
+        if (key === "id") {
+            newData[key] = uniqid()
+        }
+
+        if (key === "typeExec") {
+            newData[key] = scheme[key]
+        }
+
+        if (key === "typeView") {
+            newData[key] = scheme[key]
+        }
+
+        if (key === "name") {
+            if (!data.name) mess.push('not name')
+            else newData[key] = data[key]
+        }
+
+
+        if (key === "animationsNames") {
+            if (!data.animationsNames) mess.push('not animationsNames')
+            else if (!data.animationsNames.length === 0) mess.push('not animationsNames')
+            else newData[key] = data[key]
+        }
+
+        if (key === "armatureName") {
+            if (!data.armatureName) {
+                mess.push('not armatureName')
+            } else {
+                newData[key] = data[key]
+            }
+        }
+
+        if (key === "files") {
+            if (!data.files) {
+                mess.push('no files')
+            } else if (data.files.length === 0) {
+                mess.push('no files')
+            } else {
+                newData.files = []
+
+                for (let i = 0; i < scheme['files'].length; i++) {
+                    if (!data.files[i]['type-file']) {
+                        mess.push('no type-file')
+                    }
+
+                    if (!data.files[i]['file']) {
+                        mess.push('no file item')
+                    }
+
+                    newData['files'].push({
+                        'type-file': data.files[i]['type-file'],
+                        'file': data.files[i]['file']
+                    })
+                }
+            }
+        }
+    }
+    return { newData, mess }
+}
